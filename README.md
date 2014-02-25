8b6691423665b6c56fc0b5000e91c774e72a597c
xz6513 <xz6513@imperial.ac.uk>
2014-02-25 16:29:33 +0000

    adding framework & removing accidently added watcher

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
+<<<<<<< HEAD
+Flyway
+======
+
+Forklift Resources from One Cloud to Another
+
+TODO: Add more details
+
+Usage
+=====
+
+python flyway.py --config-file ./etc/flyway.conf
+
+
+=======
 OpenAcademy_OpenStack_Flyway
 ============================
+>>>>>>> 3212ea76e5b1b6cc0fc13117431e68b43d97e596
---
+++ flyway/flow/flow.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright (C) 2012 eBay, Inc. All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import taskflow.engines
+from taskflow.patterns import linear_flow as lf
+from taskflow.patterns import unordered_flow as uf
+
+from usertask import UserMigrationTask
+from tenanttask import TenantMigrationTask
+from roletask import RoleMigrationTask
+
+
+flow = lf.Flow('main_flow').add(
+    uf.Flow('user_tenant_migration_flow').add(
+        # Note that creating users and tenants can happen in parallel and
+        # hence it is part of unordered flow
+        UserMigrationTask('user_migration_task'),
+        TenantMigrationTask('tenant_migration_task')
+    ),
+    RoleMigrationTask('role_migration_task')
+    # TODO: Add other tasks to the flow
+)
+
+
+def execute():
+    result = taskflow.engines.run(flow, engine_conf='parallel')
+    return result
