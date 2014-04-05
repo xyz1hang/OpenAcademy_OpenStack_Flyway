bd48e3d91b2dfc17b2519dc82df7354b45832ced
Liang Shang <liang.shang13@imperial.ac.uk>
2014-04-05 12:36:00 +0100

    Add role migration task and its unit test

--- flyway/flow/roletask.py
+++ flyway/flow/roletask.py
@@ -15,11 +15,21 @@
 #    under the License.
 
 import logging
+import sys
+sys.path.append('../')
 
 from taskflow import task
+import keystoneclient.v2_0.client as ksclient
 
+from flyway.common import config as cfg
 
 LOG = logging.getLogger(__name__)
+
+
+def find_all_roles_in(keystone_client):
+    LOG.info('Get all roles in ')
+    LOG.info(keystone_client)
+    return keystone_client.roles.list()
 
 
 class RoleMigrationTask(task.Task):
@@ -28,5 +38,46 @@ class RoleMigrationTask(task.Task):
     cloud to the target cloud.
     """
 
+    def __init__(self, **kwargs):
+        super(RoleMigrationTask, self).__init__(kwargs)
+        ks_source = ksclient.Client(username=cfg.CONF.SOURCE.os_username,
+                                    password=cfg.CONF.SOURCE.os_password,
+                                    auth_url=cfg.CONF.SOURCE.os_auth_url,
+                                    tenant_name=cfg.CONF.SOURCE.os_tenant_name)
+        ks_target = ksclient.Client(username=cfg.CONF.TARGET.os_username,
+                                    password=cfg.CONF.TARGET.os_password,
+                                    auth_url=cfg.CONF.TARGET.os_auth_url,
+                                    tenant_name=cfg.CONF.TARGET.os_tenant_name)
+        self.ks_source = ksclient.Client(endpoint=cfg.CONF.SOURCE.os_keystone_endpoint,
+                                         token=ks_source.auth_token)
+        self.ks_target = ksclient.Client(endpoint=cfg.CONF.TARGET.os_keystone_endpoint,
+                                         token=ks_target.auth_token)
+
     def execute(self):
         LOG.debug('Inside role migration task...........')
+
+        source_roles = find_all_roles_in(self.ks_source)
+
+        moved_roles = self.immigrate_roles_to_target(source_roles)
+
+        LOG.info("Role immigration is finished")
+
+        return moved_roles
+
+    def immigrate_role(self, source_role):
+        target_role = self.ks_target.roles.create(source_role.name)
+        return target_role
+
+    def immigrate_roles_to_target(self, source_roles):
+        target_roles = find_all_roles_in(self.ks_target)
+        moved_roles = []
+        print source_roles
+        for role in source_roles:
+            found = False
+            for t_role in target_roles:
+                if role.name == t_role.name:
+                    found = True
+                    break
+            if not found:
+                moved_roles.append(self.immigrate_role(role))
+        return moved_roles
---
+++ flyway/tests/flow/test_roletask.py
@@ -0,0 +1,29 @@
+from testtools import TestCase
+import mox
+import sys
+import os
+sys.path.append('../..')
+from flyway.flow.roletask import RoleMigrationTask
+from flyway.common import config
+
+
+class RoleTaskTest(TestCase):
+    """Unit test for role migration"""
+
+    def __init__(self, *args, **kwargs):
+        super(RoleTaskTest, self).__init__(*args, **kwargs)
+        config.parse(['--config-file', '../../etc/flyway.conf'])
+        self.migration_task = RoleMigrationTask()
+        self.mox_factory = mox.Mox()
+
+    def test_execute(self):
+        new_role_name = "role_that_should_not_exist"
+        new_role = self.migration_task.ks_source.roles.create(new_role_name)
+
+        roles_moved_to_target = self.migration_task.execute()
+
+        target_roles = self.migration_task.ks_target.roles.list()
+        self.assertIn(roles_moved_to_target[0], target_roles)
+
+        self.migration_task.ks_source.roles.delete(new_role)
+        self.migration_task.ks_target.roles.delete(roles_moved_to_target[0])
