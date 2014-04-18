6fdce520269a732329ee6de5049f311a28695bbe
DorisXue <xuechengdoris@gmail.com>
2014-04-18 14:16:17 +0100

    Add 'projects quotas update task' and test

---
+++ flyway/flow/update_projects_quotas_task.py
@@ -0,0 +1,79 @@
+__author__ = 'chengxue'
+
+from taskflow import task
+from utils.db_handlers import tenants as db_handler
+import logging
+from utils.helper import *
+from keystoneclient import exceptions as keystone_exceptions
+from utils.resourcetype import ResourceType
+from utils import exceptions
+
+LOG = logging.getLogger(__name__)
+
+
+class UpdateProjectsQuotas(task.Task):
+    """
+    Task to update quotas for all migrated projects
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateProjectsQuotas, self).__init__(*args, **kwargs)
+
+        self.s_cloud_name = cfg.CONF.SOURCE.os_cloud_name
+        self.t_cloud_name = cfg.CONF.TARGET.os_cloud_name
+
+        self.ks_source = get_keystone_source()
+        self.ks_target = get_keystone_target()
+
+        self.nv_source = get_nova_source()
+
+    def update_quota(self, tenant_name=None, quota=None):
+        if not quota:
+            LOG.info('Nothing to be updated.')
+            return
+
+        if not tenant_name:
+            raise Exception("Tenant name cannot be null.")
+
+        ks = get_keystone_target()
+        try:
+            tenant = ks.tenants.find(name=tenant_name)
+        except keystone_exceptions.NotFound:
+            # encapsulate exceptions to make it more understandable
+            # to user. Other exception handling mechanism can be added later
+            raise exceptions.ResourceNotFoundException(
+                ResourceType.tenant, tenant_name, self.t_cloud_name)
+
+        if tenant is not None:
+            nv = get_nova_target()
+            nv.quotas.update(tenant.id,
+                             metadata_items=quota.metadata_items,
+                             injected_file_content_bytes=quota.injected_file_content_bytes,
+                             injected_file_path_bytes=None,
+                             ram=quota.ram,
+                             floating_ips=quota.floating_ips,
+                             instances=quota.instances,
+                             injected_files=quota.injected_files,
+                             cores=quota.cores,
+                             key_pairs=None,
+                             security_groups=None,
+                             security_group_rules=None)
+
+    def execute(self):
+        LOG.info('updating projects quotas ...')
+        tenants = self.ks_source.tenants.list()
+
+        for tenant in tenants:
+            tenant_name = tenant.name
+            # get the tenant data that has been migrated from src to dst
+            values = [tenant_name, self.s_cloud_name, self.t_cloud_name]
+            tenant_data = db_handler.get_migrated_tenant(values)
+
+            # only update quotas for project that has been completed migrated
+            if tenant_data is not None and tenant_data['state'] is 'completed':
+                new_name_dst = tenant_data['new_project_name']
+
+                # get source project quota
+                src_quota = self.nv_source.quotas.get(tenant.id)
+                # update destination project quota
+                self.update_quota(new_name_dst, src_quota)
