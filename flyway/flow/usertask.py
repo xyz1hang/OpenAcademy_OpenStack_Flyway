6a2f7086dfc7e11dad2849bf659e1a66639f5c06
Liang Shang <liang.shang13@imperial.ac.uk>
2014-05-14 20:22:54 +0100

    Fix a bug in revert() of usertask.

--- flyway/flow/usertask.py
+++ flyway/flow/usertask.py
@@ -33,7 +33,7 @@ class UserMigrationTask(task.Task):
         super(UserMigrationTask, self).__init__(*args, **kwargs)
         self.ks_source = get_keystone_source()
         self.ks_target = get_keystone_target()
-        self.source_users = None
+        self.migrated_users_in_source = None
 
         self.target_user_names = [user.name for user in
                                   self.ks_target.users.list()]
@@ -63,33 +63,38 @@ class UserMigrationTask(task.Task):
         Get users which are to be moved from self.ks_source.
         If param users_to_move is None, return all users in source
         """
-        return self.ks_source.users.list() if users_to_move is None \
+        return [user for user in self.ks_source.users.list()
+                if user.name not in self.target_user_names] \
+            if users_to_move is None \
             else [user for user in self.ks_source.users.list()
-                  if user.name in users_to_move]
+                  if user.name in users_to_move
+                  and user.nane not in self.target_user_names]
 
     def execute(self, users_to_move):
 
         if type(users_to_move) is list and len(users_to_move) == 0:
             return
 
-        self.source_users = self.get_source_users(users_to_move)
+        self.migrated_users_in_source = self.get_source_users(users_to_move)
 
-        initialise_users_mapping(self.source_users, self.target_user_names)
+        initialise_users_mapping(self.migrated_users_in_source, self.target_user_names)
 
         migrated_users = []
-        for user in self.source_users:
+        for user in self.migrated_users_in_source:
             migrated_user = self.migrate_one_user(user)
             if migrated_user is not None:
                 migrated_users.append(migrated_user)
 
     def revert_users(self):
+        migrated_user_names = [user.name for user in
+                               self.migrated_users_in_source]
         for user in self.ks_target.users.list():
-            if user.name in self.target_user_names:
+            if user.name in migrated_user_names:
                 self.ks_target.users.delete(user)
 
     def revert(self, *args, **kwargs):
-        if self.source_users is not None:
+        if self.migrated_users_in_source is not None:
             #Firstly delete the migrated data in target
             self.revert_users()
             #Then delete the records in DB
-            delete_all_users_mapping(self.source_users)
+            delete_all_users_mapping(self.migrated_users_in_source)
